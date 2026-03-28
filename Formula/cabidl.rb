ef8e8792717cfda907da71c89ed46e12654d892d
Per Arneng <per.arneng@scalebit.com>
2026-03-28 20:01:49 +0100

    Update cabidl to v0.3.0 and add v0.2 versioned formula

--- Formula/cabidl.rb
+++ Formula/cabidl.rb
@@ -1,26 +1,26 @@
 class Cabidl < Formula
   desc "Component Architecture Boundary and Interface Definition Language"
   homepage "https://github.com/scalebit-com/cabidl"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/scalebit-com/cabidl/releases/download/v0.2.0/cabidl-0.2.0-macos-arm64.tar.gz"
-      sha256 "29df2d27667d52d8433bdf2309b44bcc853e7fdcf8839d9c3a056ceface6a17b"
+      url "https://github.com/scalebit-com/cabidl/releases/download/v0.3.0/cabidl-0.3.0-macos-arm64.tar.gz"
+      sha256 "0422a0d9bfcbcb099bc37b394e0d8277f4984ec36cd51a69edaf06982962a92f"
     else
-      url "https://github.com/scalebit-com/cabidl/releases/download/v0.2.0/cabidl-0.2.0-macos-x86_64.tar.gz"
-      sha256 "896e1ec585fa3d36ccb2db373c56467c4a4bcfbfb1999afa0b2d749293cf8c2e"
+      url "https://github.com/scalebit-com/cabidl/releases/download/v0.3.0/cabidl-0.3.0-macos-x86_64.tar.gz"
+      sha256 "1dc3b3922b3e1fe02bf749c00996c1ef4c7f6cdb94be99c52654e43afe884681"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/scalebit-com/cabidl/releases/download/v0.2.0/cabidl-0.2.0-linux-arm64.tar.gz"
-      sha256 "06364e538a55022b94ec28ff004b7488589afd7b407aceba1ff87c56ca68d0e7"
+      url "https://github.com/scalebit-com/cabidl/releases/download/v0.3.0/cabidl-0.3.0-linux-arm64.tar.gz"
+      sha256 "c6b35d28b22d1ccd73590b25ac2f1934f8176ece62c2bb17b51ef50aed0ac457"
     else
-      url "https://github.com/scalebit-com/cabidl/releases/download/v0.2.0/cabidl-0.2.0-linux-x86_64.tar.gz"
-      sha256 "da2e78e4e71d6656d7ec1727101a2f385f0cbc826ed5a0b0d523753b85463319"
+      url "https://github.com/scalebit-com/cabidl/releases/download/v0.3.0/cabidl-0.3.0-linux-x86_64.tar.gz"
+      sha256 "4b3900dbce6dce2b3a4e636b909f05c48ab303e245f243cbbf8d60b75dd4400a"
     end
   end
 
